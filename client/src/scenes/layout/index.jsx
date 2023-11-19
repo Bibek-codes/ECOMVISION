85b0b769f4c01d12e349cb02fee0d2742c5587ec
Bibek-codes <bibekpanda07@gmail.com>
2023-11-19 20:54:49 +0530

    initial commit

---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar />
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
